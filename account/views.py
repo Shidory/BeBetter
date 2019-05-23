5b6db4b0b9ef5b3c4fafc04b1a6bb0edc878cf40
Simon MWEPU <simonmwepu@gmail.com>
2019-05-23 12:50:49 +0200

    [ADD]login request

--- account/views.py
+++ account/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
-class Account():
-    def login(self):
-        pass
+"""class Account():
+    def login(self, request):
+        return render(request, 'account/login.html')"""
+def login(request):
+    return render(request, 'account/login.html')
