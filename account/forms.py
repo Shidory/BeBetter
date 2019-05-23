f8cc3b23df7e222c8d4dcd550d5f731d1bded150
Simon MWEPU <simonmwepu@gmail.com>
2019-05-23 16:28:33 +0200

    [ADD]form_validation

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 from .models import *
 
-class LoginForm(forms.ModelForm):
-    pass
+class UserLoginForm(forms.ModelForm):
+    email = forms.EmailField(widget=forms.EmailField(), require=True, max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class meta:
+        model = UserModel
+        fields = ['email', 'password']
