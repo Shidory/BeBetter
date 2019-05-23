6793aa08fda9e778d6891a2dc69951a714ed660c
Simon MWEPU <simonmwepu@gmail.com>
2019-05-23 15:16:10 +0200

    [UPDATE]fix args bug

--- account/views.py
+++ account/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 # Create your views here.
-"""class Account():
-    def login(self, request):
-        return render(request, 'account/login.html')"""
-def login(request):
-    return render(request, 'account/login.html')
+class Account():
+    def login(request):
+        return render(request, 'account/login.html')
