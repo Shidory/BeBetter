650047c0ae6b9f06d35ee44b11755b6336024e53
Simon MWEPU <simonmwepu@gmail.com>
2019-05-23 16:27:07 +0200

    [ADD]verify validity of form

--- account/views.py
+++ account/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from . forms import *
 
 # Create your views here.
 class Account():
     def login(request):
-        return render(request, 'account/login.html')
+        if request.method == 'POST':
+            form = UserLoginForm(request.POST)
+            if form.is_valid():
+                form.save()
+        #return render(request, 'account/login.html')
