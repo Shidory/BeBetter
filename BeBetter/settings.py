55ec1c5f7195806bf1ef00b5388ddae608927a79
Simon MWEPU <simonmwepu@gmail.com>
2019-05-23 12:11:54 +0200

    [ADD]DIRS for account templates

--- BeBetter/settings.py
+++ BeBetter/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'BeBetter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['account/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
