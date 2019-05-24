47d735eb2d891391c2613f32acf683673b8729f8
Simon MWEPU <simonmwepu@gmail.com>
2019-05-24 06:07:47 +0200

    [ADD]email fiels

--- account/models.py
+++ account/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class UserLogin(models.Model):
-    pass
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.email
