da0209c009c02b1c0adb6ab8efd5e494fafea705
Simon MWEPU <simonmwepu@gmail.com>
2019-05-24 13:15:55 +0200

    [UPDATE]Account to AccountLogin

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Account.login, name = 'login'),
+    path('', views.AccountLogin.login, name='login'),
+    path('', views.Account.login, name='signup'),
 ]
