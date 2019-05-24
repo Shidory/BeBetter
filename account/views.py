d528039a09dbec7aa9a2ada96126c280a88018ea
Simon MWEPU <simonmwepu@gmail.com>
2019-05-24 06:08:35 +0200

    [ADD]requests

--- account/views.py
+++ account/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
-from . forms import *
+from django.http import *
+from .forms import *
+from .models import *
 
-# Create your views here.
 class Account():
     def login(request):
         if request.method == 'POST':
             form = UserLoginForm(request.POST)
             if form.is_valid():
                 form.save()
-        #return render(request, 'account/login.html')
+                return HttpResponseRedirect('/account/')
+        else:
+            form = UserLoginForm()
+        return render(request, 'account/login.html', {'form':form})
