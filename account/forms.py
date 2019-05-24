914ef9327f8f60cd9cd05a011e5bd326921dcb2c
Simon MWEPU <simonmwepu@gmail.com>
2019-05-24 08:25:28 +0200

    [FIX]unexpeted keyword 'require' bug

--- account/forms.py
+++ account/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from .models import *
 
 class UserLoginForm(forms.ModelForm):
-    email = forms.EmailField(widget=forms.EmailField(), require=True, max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput, require=True, max_length=32)
+    email = forms.EmailField(widget=forms.EmailField(), max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput, max_length=32)
 
     class Meta:
         model = UserLogin
