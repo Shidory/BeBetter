1b070122757f73f1479561b34f4668fbfac99e78
Simon MWEPU <simonmwepu@gmail.com>
2019-05-24 14:19:09 +0200

    [ADD]redirection

--- account/views.py
+++ account/views.py
@@ -16,4 +16,19 @@ class Account:
         return render(request, 'account/login.html', {'form':form})
 
     def signup(request):
-        pass
+        if request.method == 'POST':
+            form = UserSignUpForm(request.POST)
+            if form.is_valid():
+                first_name = form.cleaned_data['first_name']
+                last_name = form.cleaned_data['last_name']
+                username = form.cleaned_data['username']
+                email = form.cleaned_data['email']
+                password = form.cleaned_data['password']
+                User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+                messages.success(request, 'User registration successful')
+                usr = auth.authentifacte(username=username, password=password)
+                auth.login(request, usr)
+                return render(request, 'account/login')
+        else:
+            form = UserSignUpForm()
+        return render(request, 'registration.html', {'frm': form})
