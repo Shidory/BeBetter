7ab0aaddf613b9b18f2f19731e2b4369e9b5ef44
Simon MWEPU <simonmwepu@gmail.com>
2019-05-24 06:07:08 +0200

    [UPDATE]Meta class

--- account/forms.py
+++ account/forms.py
@@ -3,8 +3,8 @@ from .models import *
 
 class UserLoginForm(forms.ModelForm):
     email = forms.EmailField(widget=forms.EmailField(), require=True, max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput, require=True, max_length=32)
 
-    class meta:
-        model = UserModel
+    class Meta:
+        model = UserLogin
         fields = ['email', 'password']
