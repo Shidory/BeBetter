42e85567522c74397ff8dd6f3249a8dd68496a92
Simon MWEPU <simonmwepu@gmail.com>
2019-05-24 20:50:18 +0200

    [UPDATE]enable User

--- account/models.py
+++ account/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 class UserLogin(models.Model):
     email = models.EmailField(max_length=50)
