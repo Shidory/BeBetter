e2dfe41487722c9a829494cfdab39360af1db48b
Simon MWEPU <simonmwepu@gmail.com>
2019-05-25 08:04:19 +0200

    [ADD]config db

--- BeBetter/settings.py
+++ BeBetter/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'BeBetter.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'bb.db'),
     }
 }
 
