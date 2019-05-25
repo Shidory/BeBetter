1d91760a72246f0b83d53055ae3cf5aaaa015da3
Simon MWEPU <simonmwepu@gmail.com>
2019-05-25 17:35:44 +0200

    [ADD]__str__ return

--- account/models.py
+++ account/models.py
@@ -14,3 +14,6 @@ class UserSignUp(models.Model):
     username = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.email
